4aab271fbc172bcf2004b3267e640b53c5df5852
Sarah R <phoesarah@hotmail.com>
2015-06-09 14:36:06 -0500

    Tried to change date on Contacts to not show time, failed miserably

--- ProspectManager.Mvc/Models/Contacts/Contact.cs
+++ ProspectManager.Mvc/Models/Contacts/Contact.cs
@@ -15,6 +15,15 @@ namespace ProspectManager.Mvc.Models.Contacts
         [EmailAddress]
         public string Email { get; set; }
 
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime BirthDate { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        public string ReturnDateForDisplay
+        {
+            get { return this.BirthDate.ToString(); }
+        }
     }
 }
